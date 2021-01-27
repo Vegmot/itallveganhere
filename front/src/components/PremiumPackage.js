89896d480f8ae5b311486595ef0ec7bf5e75d9f6
Vegmot <anthony.yh.jang@gmail.com>
2021-01-27 15:10:55 -0800

    added a little bit of styling(css)

--- front/src/components/PremiumPackage.js
+++ front/src/components/PremiumPackage.js
@@ -5,14 +5,23 @@ import { Card, ListGroup } from 'react-bootstrap';
 const PremiumPackage = ({ premiumPackage }) => {
   return (
     <>
-      <Card>
-        <Card.Img variant='top' src={premiumPackage.image} />
+      <Card className='my-5 premium-card'>
+        <ListGroup.Item className='premium-image'>
+          <Card.Img variant='top' src={premiumPackage.image} rounded />
+        </ListGroup.Item>
         <Card.Body>
-          <Card.Title>{premiumPackage.name}</Card.Title>
+          <Card.Title>
+            <strong>{premiumPackage.name}</strong>
+          </Card.Title>
+          <Card.Text>$ {premiumPackage.price}</Card.Text>
           <Card.Text>{premiumPackage.description}</Card.Text>
         </Card.Body>
         <ListGroup>
-          <ListGroup.Item>$ {premiumPackage.price}</ListGroup.Item>
+          <ListGroup.Item>
+            <Link to='/' className='btn btn-primary btn-block'>
+              Add to cart (+ $ {premiumPackage.price})
+            </Link>
+          </ListGroup.Item>
         </ListGroup>
       </Card>
     </>
--- front/src/components/Product.js
+++ front/src/components/Product.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
 
 const Product = props => {
   return (
     <>
-      <Card className='my-3 p-3 rounded'>
-        <Link to={`/products/${props.product._id}`}>
-          <Card.Img src={props.product.image} variant='top' />
-        </Link>
-
-        <Card.Body>
+      <LinkContainer
+        to={`/products/${props.product._id}`}
+        className='product-item-card-outline'
+      >
+        <Card className='my-3 p-3 rounded product-item-card'>
           <Link to={`/products/${props.product._id}`}>
-            <Card.Title as='div'>
-              <strong>{props.product.name}</strong>
-            </Card.Title>
+            <Card.Img src={props.product.image} variant='top' />
           </Link>
 
-          <Card.Text as='div'>
-            <Rating
-              value={props.product.rating}
-              text={`${props.product.numReviews} reviews`}
-            />
-          </Card.Text>
+          <Card.Body>
+            <Link to={`/products/${props.product._id}`}>
+              <Card.Title as='div'>
+                <strong>{props.product.name}</strong>
+              </Card.Title>
+            </Link>
 
-          <Card.Text as='h3'>$ {props.product.price}</Card.Text>
-        </Card.Body>
-      </Card>
+            <Card.Text as='div'>
+              <Rating
+                value={props.product.rating}
+                text={`${props.product.numReviews} reviews`}
+              />
+            </Card.Text>
+
+            <Card.Text as='h3'>$ {props.product.price}</Card.Text>
+          </Card.Body>
+        </Card>
+      </LinkContainer>
     </>
   );
 };
